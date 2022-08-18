62dea9f34d3da529ac25d2f4e1994f52968df899
Geraldo Luiz <geraldobl58@gmail.com>
2022-08-18 19:26:48 -0300

    :sparkles: Customizing inputs

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -19,14 +19,26 @@ export function Home() {
           <TaskInput
             type="text"
             id="task"
+            list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
           />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+            <option value="Projeto 5" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutes.</span>
